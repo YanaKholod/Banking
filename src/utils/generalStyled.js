a66306dad2432df9e0f541e88b6ef48f4629a76e
YanaKholod <106437450+YanaKholod@users.noreply.github.com>
2023-10-02 11:36:26 +0300

    upd media

--- src/utils/generalStyled.js
+++ src/utils/generalStyled.js
@@ -10,6 +10,8 @@ export const StyleWrapper = styled.div`
   flex-direction: column;
   margin: 0px 80px;
   padding: 24px 0;
+  height: 100vh;
+  margin-bottom: 10px;
   @media (max-width: 850px) {
     margin: 0px 10px;
   }
